f66462e7c2f769b6153be9470c35ea9a59c632a5
mtcerv23 <mtcerv23@gmail.com>
2023-12-11 17:27:22 -0800

    remote

---
+++ script.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector('.modal');
+const openButton = document.querySelector('.open-button');
+const closeButton = document.querySelector('.close-button');
+
+openButton.addEventListener('click', () => {
+    modal.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
